972ea066444f7c39999e0398bfcbb4820a1eb5fd
Ranjith Sekar <ranjith.sekar@clarivate.com>
2020-02-04 19:15:25 +0530

    update

---
+++ springcore-collections/src/main/java/springcore/collections/xml/AppXmlMail.java
@@ -0,0 +1,17 @@
+package springcore.collections.xml;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import springcore.collections.model.Mobile;
+
+public class AppXmlMail {
+
+  public static void main(String[] args) {
+    ApplicationContext context = new ClassPathXmlApplicationContext("spring-core-collections-beans.xml");
+    Mobile mobile = (Mobile) context.getBean("mobile");
+    mobile.printMobileList();
+    mobile.printPriceMap();
+    mobile.printModelSet();
+  }
+}
