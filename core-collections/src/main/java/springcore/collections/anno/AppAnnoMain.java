aac2b3ccb60566b241ccf35293823d337fbc4b19
Ranjith Sekar <ranjith.sekar@clarivate.com>
2020-02-29 21:29:11 +0530

    update

---
+++ core-collections/src/main/java/springcore/collections/anno/AppAnnoMain.java
@@ -0,0 +1,19 @@
+package springcore.collections.anno;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import springcore.collections.model.Product;
+
+public class AppAnnoMain {
+
+  public static void main(String[] args) {
+    ApplicationContext context = new AnnotationConfigApplicationContext(AppAnnoConfig.class);
+
+    Product product = (Product) context.getBean(Product.class);
+    System.out.println(product.gettProductList());
+    System.out.println(product.getPriceMap());
+    System.out.println(product.getModelSet());
+  }
+
+}
